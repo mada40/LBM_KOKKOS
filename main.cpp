43d22bee3afd435b56444a7f43eea5d7845cd58e
4mada0 <4mada0@gmail.com>
2023-05-16 06:35:17 +0300

    new main

--- main.cpp
+++ main.cpp
@@ -1,51 +1,64 @@
 #include"BoltzmannSimulation.h"
 #include<iostream>
-#include <omp.h>
+#include <fstream>
+#include <iomanip>
 #include <Kokkos_Core.hpp>
 
 
 // argv = "W H GIRD_SIZE NUMBER_OF_ITERATIONS NUM_THREADS"
 int main(int argc, char** argv)
 {
-    Kokkos::initialize(
-        Kokkos::InitializationSettings()
-        .set_disable_warnings(false)
-        .set_num_threads(1));
-    int W = 11000;
-    int H = 11000;
-    int NUMBER_OF_ITERATIONS = 3;
-
-
-
-    if (argc != 1 && argc != 4)
+    Kokkos::initialize(argc, argv);
     {
-        Kokkos::finalize();
-        return -1;
+        int W = 110;
+        int H = 110;
+        int NUMBER_OF_ITERATIONS = 3;
+
+
+
+        if (argc != 1 && argc != 4 && argc != 5)
+        {
+            Kokkos::finalize();
+            return -1;
+        }
+
+        if (argc > 1)
+        {
+            W = atoi(argv[1]);
+            H = atoi(argv[2]);
+            NUMBER_OF_ITERATIONS = atoi(argv[3]);
+        }
+
+
+
+        BoltzmannSumulation bs(W, H);
+        Kokkos::Timer timer;
+
+        timer.reset();
+        double start = timer.seconds();
+        bs.update(NUMBER_OF_ITERATIONS);
+        Kokkos::fence();  // synchronize
+
+
+        if (argc == 5)
+        {
+            freopen(argv[4], "w", stdout);
+
+            std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+            std::cout << std::fixed << std::showpoint;
+            std::cout << std::setprecision(8);
+
+            auto table = bs.get_table();
+            for (int l = 0; l < 9; ++l)
+                for (int i = 0; i < W * H; ++i)
+                    std::cout << table(i, l) << "\n";
+
+        }
+        else
+        {
+            std::cout << timer.seconds() - start << std::endl;
+        }
     }
-
-    if (argc > 1)
-    {
-        W = atoi(argv[1]);
-        H = atoi(argv[2]);
-        NUMBER_OF_ITERATIONS = atoi(argv[3]);
-    }
-
-
-
-    BoltzmannSumulation bs(W, H);
-
-    double start = omp_get_wtime();
-    for (int i = 0; i < NUMBER_OF_ITERATIONS; i++)
-    {
-        bs.update();
-        //std::cout << i << "\n";
-    }
-    double end = omp_get_wtime();
-
-    //freopen("awdef.txt", "w", stdout);
-    std::cout << end - start << " ";
-
-
     Kokkos::finalize();
 
     return 0;
